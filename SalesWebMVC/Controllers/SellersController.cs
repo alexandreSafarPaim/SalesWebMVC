5b8e146336af58c1876aeb872d7bf32eed6e1657
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-03-20 13:23:59 -0300

    Delete Seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -25,6 +25,7 @@ namespace SalesWebMVC.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+        [HttpGet]
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
@@ -37,6 +38,29 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
