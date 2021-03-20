914b2e07c39623f148523d874fede2c36c534b5c
alexandreSafarPaim <alexandresafarpaim@gmail.com>
2021-03-20 12:27:08 -0300

    Foreign fey not null (referencial integrity)

--- SalesWebMVC/Migrations/20210320152215_DepartmentForeiKey.Designer.cs
+++ SalesWebMVC/Migrations/20210320152215_DepartmentForeiKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMVC.Data;
 
 namespace SalesWebMVC.Migrations
 {
     [DbContext(typeof(SalesWebMVCContext))]
-    partial class SalesWebMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210320152215_DepartmentForeiKey")]
+    partial class DepartmentForeiKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
